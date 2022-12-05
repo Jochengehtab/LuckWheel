84c58adf2f7eb4c8562b4191de415eba774de7a6
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2022-12-05 18:13:57 +0100

    Finaly made the Lucky Wheel include from a GitHub

--- Wheel/app/src/main/java/com/example/Jochengehtab/wheel/MainActivity.java
+++ Wheel/app/src/main/java/com/example/Jochengehtab/wheel/MainActivity.java
@@ -2,38 +2,48 @@ package com.example.Jochengehtab.wheel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.KeyEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
+
+import com.bluehomestudio.luckywheel.LuckyWheel;
+import com.bluehomestudio.luckywheel.WheelItem;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import kotlin.jvm.internal.Ref;
 
+@SuppressWarnings("unused")
 public class MainActivity extends AppCompatActivity {
-
+    private final String yellow = "#ffff00", blue = "#00008B", red = "#FF0000", green = "#008000";
+    private LuckyWheel lw;
+    List<WheelItem> wheelItems;
     private Button button;
-    private CountDownTimer timer;
     private int max, min;
-    private List<String> members = new ArrayList<>();
+    private final List<String> members = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        min = 10;
-        max = 30;
+
+        defaultWheel();
+        lw = findViewById(R.id.lwv);
+
+        min = 0;
 
         button = findViewById(R.id.btnSpin);
-
         Random random = new Random();
+
+        lw.addWheelItems(wheelItems);
+        lw.setTarget(1);
+
+        lw.setLuckyWheelReachTheTarget(() -> showDefaultSubtitle("Target Reached"));
 
         EditText editText = findViewById(R.id.input);
 
@@ -44,45 +54,66 @@ public class MainActivity extends AppCompatActivity {
                     return false;
                 }
                 members.add(editText.getText().toString());
+                addAItem(editText.getText().toString());
                 showDefaultSubtitle(editText.getText().toString() + " wurde hinzugefügt.");
             }
             return false;
         });
+
+
         button.setOnClickListener(view -> {
 
-            button.setEnabled(false);
+            if (members.size() == 0){
+                showDefaultSubtitle("Bitte gib ein paar Namen ein!");
+                return;
+            }
+            max = members.size();
 
-            int spin = random.nextInt(max) + min;
+            button.setEnabled(false);
+            int randomNumber = random.nextInt(max) + min;
 
             //Show Subtitle
             showDefaultSubtitle("Ausloshung...");
 
-            //Since the wheel has 10 divisions, the
-            //Rotation should be a multiple of
-            //360/10 = 36 degrees
-            spin = spin * 36;
-
-            //Timer for each degree movement
-            int finalSpin = spin;
-            timer = new CountDownTimer(finalSpin,1) {
-
-                @Override
-                public void onTick(long l) {
-
-                }
-
-                @Override
-                public void onFinish() {
-                    //Enabling the Button when it finished
-                    button.setEnabled(true);
-                }
-            }.start();
-
+            lw.rotateWheelTo(1);
+            button.setEnabled(true);
         });
 
+    }
+
+    private void defaultWheel(){
+        wheelItems = new ArrayList<>();
+        wheelItems.add(new WheelItem(Color.parseColor(blue), BitmapFactory.decodeResource(getResources(), R.drawable.blue), "Enter to stat :)"));
+    }
+
+    private void addAItem(String name){
+        wheelItems.add(new WheelItem(Color.parseColor(getRandomColor()), BitmapFactory.decodeResource(getResources(), R.drawable.yellow), name));
+    }
+
+    private String getRandomColor(){
+        Random random = new Random();
+        int minRange = 0, maxRange = 3, randomNumber = random.nextInt( maxRange + 1 - minRange) + minRange;
+        String color = blue;
+
+        switch (randomNumber){
+            case 0:
+                color = blue;
+                break;
+            case 1:
+                color = yellow;
+                break;
+            case 2:
+                color = red;
+                break;
+            case 3:
+                color = green;
+                break;
+        }
+        return color;
     }
 
     private void showDefaultSubtitle(String msg){
         Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
     }
+
 }
