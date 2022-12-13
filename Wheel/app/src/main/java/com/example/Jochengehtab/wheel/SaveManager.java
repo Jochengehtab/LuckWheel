2f4ac3b6972f4034b7bf42f64d5496650b3482ea
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2022-12-13 12:25:03 +0100

    Update SaveManager.java

--- Wheel/app/src/main/java/com/example/Jochengehtab/wheel/SaveManager.java
+++ Wheel/app/src/main/java/com/example/Jochengehtab/wheel/SaveManager.java
@@ -7,8 +7,10 @@ public class SaveManager {
 
     private final SharedPreferences sharedPreferences;
     private final SharedPreferences.Editor editor;
+    private Context context;
 
     public SaveManager(Context context){
+        this.context = context;
         this.sharedPreferences = context.getSharedPreferences("Classes", 0);
         this.editor = sharedPreferences.edit();
     }
@@ -16,6 +18,11 @@ public class SaveManager {
     public SaveManager(Context context, String name){
         this.sharedPreferences = context.getSharedPreferences(name, 0);
         this.editor = sharedPreferences.edit();
+    }
+    
+    public SharedPreferences create(String name){
+        this.sharedPreferences = context.getSharedPreferences(name, 0);
+        return this.sharedPreferences;
     }
 
     public String get(String key){
